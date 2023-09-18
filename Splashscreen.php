db38183e18c2e1864d71df9a9bcfe0b42517b967
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-18 09:40:45 +0700

    spalshscreen baru

--- Splashscreen.php
+++ Splashscreen.php
@@ -1,7 +1,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.12111">
+    <meta> M_1_PI 
     <title>Document</title>
 </head>
 <body>
