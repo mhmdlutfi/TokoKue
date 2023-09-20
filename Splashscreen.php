b696929c34031824ad22d4f0b8f494353fb49d92
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-20 13:48:19 +0700

    Penambahan Style.css di splash screen

--- Splashscreen.php
+++ Splashscreen.php
@@ -1,15 +1,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
-    <meta name="viewport" content="width=device-width, initial-scale=1.0aaa">
-=======
     <meta name="viewport" content="width=device-width, initial-scale=1.12111">
     <meta> M_1_PI 
->>>>>>> login_user
     <title>Document</title>
 </head>
 <body>
     <h1>testing 112</h1>
+    <p>ajakan yang sangat anjay men () yes or no </p>
+    <style type="text/css" >
+        p { font-family: serif;
+            line-height: 2em;
+            font-size: small;
+        }
+        i { 
+            font-family: sans-serif;
+            color: red;
+        }
+    </style>
 </body>
 </html>
