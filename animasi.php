2776f6013ae6dcb308e2d0d1a3746d5266895e37
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-29 10:25:42 +0700

    add baru

--- animasi.php
+++ animasi.php
@@ -5,6 +5,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>hayang</h1>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>muhamad lutfi</h1>
+    </body>
+    </html>
 </body>
 </html>
